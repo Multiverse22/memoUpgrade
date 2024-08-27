fb4af2f63e4dfb492686d0cec30c3ca0685ef7d9
multiverse22 <momizi527@gmail.com>
2024-08-27 13:54:17 +0900

    init commit And 환경변수 세팅

---
+++ src/main/java/com/sparta/memoupgrade/MemoUpgradeApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.memoupgrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoUpgradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoUpgradeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/memoupgrade/MemoUpgradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.memoupgrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoUpgradeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
